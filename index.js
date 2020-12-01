6a2ac332e87237089cf4cb96cb47b80b07ade3bd
AironDev <airondev@gmail.com>
2020-12-01 05:05:24 +0100

    done site scrapper

---
+++ index.js
@@ -0,0 +1,24 @@
+// index.js
+const scrape = require('website-scraper');
+const PuppeteerPlugin = require('website-scraper-puppeteer');
+const path = require('path');
+
+scrape({
+    // Provide the URL(s) of the website(s) that you want to clone
+    urls: ['https://code-theme.com/html/findhouses/index-17.html'],
+    // Specify the path where the content should be saved
+    directory: path.resolve(__dirname, 'sites/findhouses_demo17'),
+    // Load the Puppeteer plugin
+    plugins: [ 
+        new PuppeteerPlugin({
+            launchOptions: { 
+                // If you set  this to true, the headless browser will show up on screen
+                headless: true
+            }, /* optional */
+            scrollToBottom: {
+                timeout: 10000, 
+                viewportN: 10 
+            } /* optional */
+        })
+    ]
+});
